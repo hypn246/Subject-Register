dc2c5756879c6b435f2af7d85830120d32fdc099
hypn <khangha2406@gmail.com>
2026-04-25 14:35:47 +0700

    Edit over func, enroll view ok

--- ExtensionDKM/Controllers/EnrollController.cs
+++ ExtensionDKM/Controllers/EnrollController.cs
@@ -1,9 +1,10 @@
 ﻿using ExtensionDKM.Data;
-using Microsoft.AspNetCore.Mvc;
+using ExtensionDKM.DTOs;
 using ExtensionDKM.Models;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace ExtensionDKM.Controllers
 {
@@ -18,12 +19,39 @@ namespace ExtensionDKM.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            var classroom = await _context.Classrooms
-                .Include(c => c.Course)
-                .Include(c => c.Lecturer)
-                .Include(c => c.Room)
+            //var classrooms = await _context.Classrooms
+            //    .AsSplitQuery()
+            //    .Include(c => c.Course)
+            //        .ThenInclude(c => c.PreviousCourses)
+            //    .Include(c => c.Course)
+            //        .ThenInclude(c => c.RequirementCourses)
+            //    .Include(c => c.Lecturer)
+            //    .Include(c => c.Room)
+            //    .ToListAsync();
+            IEnumerable<ClassrommDTO> classrooms = await _context.Classrooms
+                .Select(c => new ClassrommDTO
+                {
+                    Id = c.Id,
+                    Time = c.Time,
+                    SchoolYear = c.SchoolYear,
+                    Semester = c.Semester,
+                    SS = c.SS,
+
+                    CourseName = c.Course.Name,
+                    LecturerName = c.Lecturer.Name,
+                    RoomName = c.Room.Name,
+
+                    PreviousCourses = c.Course.PreviousCourses
+                .Select(x => x.PreviousCourse.Name)
+                .ToList(),
+
+                    RequirementCourses = c.Course.RequirementCourses
+                .Select(x => x.RequirementCourse.Name)
+                .ToList()
+                })
                 .ToListAsync();
-            return View(classroom);
+
+            return View(classrooms);
         }
     }
 }
